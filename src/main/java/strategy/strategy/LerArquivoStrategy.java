0213035d2eda2ce375986ce28a1f157995653cea
Wilker Bruno <wilkerbruno16@gmail.com>
2021-10-25 20:51:07 -0300

    feat: adicionando interface Strategy

---
+++ src/main/java/strategy/strategy/LerArquivoStrategy.java
@@ -0,0 +1,10 @@
+package strategy.strategy;
+
+import strategy.mapper.MapearParaPedido;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface LerArquivoStrategy<T extends MapearParaPedido> {
+    List<T> lerArquivo(InputStream arquivoInputStream);
+}
